3ac32ca7658e9fd0d72c027b2dc78ba16a737758
jessyon <kateyon@gmail.com>
2017-03-23 14:04:33 +1000

    Added paperclip for movie image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
 	belongs_to :user
+
+
+  has_attached_file :image, styles: { medium: "400x600>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
